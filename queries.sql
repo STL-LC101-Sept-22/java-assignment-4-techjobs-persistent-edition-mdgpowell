1021fe778a2d3f7c7afff5b3596de4b16b81f26a
mdgpowell <mdgpowell@gmail.com>
2023-03-27 21:22:26 -0500

    finished assignment, last sqlquery not working

--- queries.sql
+++ queries.sql
@@ -1,7 +1,13 @@
 -- Part 1: Test it with SQL
-
+-- SELECT * from job;
 -- Part 2: Test it with SQL
-
+SELECT name from employer WHERE location = "St. Louis City";
 -- Part 3: Test it with SQL
-
+DROP TABLE job;
 -- Part 4: Test it with SQL
+SELECT name from skills WHERE id IN (SELECT skills_id FROM job_skills) ORDER BY name;
+
+SELECT * FROM skill
+LEFT JOIN job_skills ON (skill.id = job_skills.skills_id)
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
